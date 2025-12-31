d760e47dcd2b5feafce5965baa322cd4a84d73ca
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2025-12-31 14:43:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const ul=document.querySelector("#uql");
+
+const bands = ['The Plot in You', 'The Devil Wears Prada',
+			   'Pierce the Veil', 'Norma Jean', 'The Bled',
+			   'Say Anything', 'The Midway State',
+			   'We Came as Romans', 'Counterparts', 'Oh, Sleeper',
+			   'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+const sorted=bands.sort((a,b)=>{
+	const normalise=str=>
+		str.toLowerCase().replace(/^(a |an |the )/i,"");
+	return normalise(a).toLocaleCompare(normalise(b));
+})
+
+for(let i=0;i<sorted.length;i++){
+	 const li = document.createElement("li");
+  li.textContent = sorted[i];
+  ul.appendChild(li);
+}
